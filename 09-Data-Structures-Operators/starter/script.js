85ddb06748db5cb4de929ce387f5a27803d3e023
Guillaume <breyerguillaume@gmail.com>
2022-11-29 22:09:16 +0100

    init Coding Challenge #1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,98 +1,123 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// // Data needed for first part of the section
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    adress,
-  }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`
-    );
-  },
+//   orderDelivery: function ({
+//     starterIndex = 1,
+//     mainIndex = 0,
+//     time = '20:00',
+//     adress,
+//   }) {
+//     console.log(
+//       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`
+//     );
+//   },
 
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is your pasta with ${ing1}, ${ing2}, and ${ing3}`);
-  },
+//   orderPasta: function (ing1, ing2, ing3) {
+//     console.log(`Here is your pasta with ${ing1}, ${ing2}, and ${ing3}`);
+//   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
+//   orderPizza: function (mainIngredient, ...otherIngredients) {
+//     console.log(mainIngredient);
+//     console.log(otherIngredients);
+//   },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
 
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-};
+// const rest1 = {
+//   name: 'Capri',
+//   numGuests: 20,
+// };
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests);
+// const rest2 = {
+//   name: 'La piazza',
+//   owner: 'Rosy',
+// };
 
-// nullish: null and undefined (not 0 or '')
-const guestCorrect = restaurant.numGuests ?? 10;
+// // OR assignement operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// // equals to : (it assigns first condition if it's true. If not, the second one)
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+// // but be carefull because 0 is considered falsy
+// // below solution with the NULLISH assignement operator : (null or undefined)
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
 
-// Use any data type, return any data type, short-circuiting // && and || OPERATORS
-  // ||
-  // ShortCut when the first value is true
-  .console
-    .log(3 || 'guillaume');
-console.log('' || 'guillaume');
-console.log(true || 0);
-console.log(undefined || null);
+// // && assignement operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>'; // RETURN UNDEFINED
+// rest2.owner = rest2.owner && '<ANONYMOUS>'; // RETURN <ANONYMOUS>
+// // equals to :
+// rest1.owner && '<ANONYMOUS>';
+// rest2.owner && '<ANONYMOUS>';
 
-console.log(undefined || 0 || '' || 'hello' || 23 || null);
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
 
-// restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+// // nullish: null and undefined (not 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10;
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+// // Use any data type, return any data type, short-circuiting // && and || OPERATORS
+// // ||
+// // ShortCut when the first value is true
+// console.log('' || 'guillaume');
+// console.log(true || 0);
+// console.log(undefined || null);
 
-// &&
-// ShortCut when the first value is falsy (so lastOne if not)
-console.log(3 && 'guillaume');
-console.log('' && 'guillaume');
-console.log(true && 0);
-console.log(undefined && null);
+// console.log(undefined || 0 || '' || 'hello' || 23 || null);
 
-console.log('hello' && 23 && null && 'guillaume' && 23);
+// // restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
 
-// Pratical example
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
-}
-// equals to :
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// // &&
+// // ShortCut when the first value is falsy (so lastOne if not)
+// console.log(3 && 'guillaume');
+// console.log('' && 'guillaume');
+// console.log(true && 0);
+// console.log(undefined && null);
+
+// console.log('hello' && 23 && null && 'guillaume' && 23);
+
+// // Pratical example
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+// // equals to :
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 ///////////////////////////////////////
 // Rest pattern and rest parameters
@@ -250,3 +275,23 @@ restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 // // default values
 // const [p = 1, q = 1, r = 1] = [8, 9];
+
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
